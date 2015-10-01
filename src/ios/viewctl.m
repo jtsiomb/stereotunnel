c4341ac7b67369339b052297207f6fb8db29bbb1
John Tsiombikas <nuclear@member.fsf.org>
2015-10-01 07:54:57 +0300

    ios version done

--- src/ios/viewctl.m
+++ src/ios/viewctl.m
@@ -5,11 +5,15 @@
 
 @interface ViewController () {
 	EAGLContext *ctx;
-	float pixel_scale;
+	float pixel_scale, max_pixel_scale, low_pixel_scale;
 
 	GLKView *glview;
 	ADBannerView *ad;
 	BOOL ad_visible;
+	BOOL ad_ready;
+
+	int ios_ver_major, ios_ver_minor;
+	BOOL landscape_swap;
 }
 
 
@@ -19,7 +23,54 @@
 
 - (void)init_gl;
 - (void)destroy_gl;
+
+- (void)enable_retina;
+- (void)disable_retina;
+- (BOOL)is_retina_enabled;
+- (BOOL)have_retina;
 @end
+
+static ViewController *view_ctl;
+static BOOL ad_show_requested;
+
+void ad_banner_show(void)
+{
+	if(view_ctl) {
+		[view_ctl show_ad];
+	}
+	ad_show_requested = YES;
+}
+
+void ad_banner_hide(void)
+{
+	if(view_ctl) {
+		[view_ctl hide_ad];
+	}
+	ad_show_requested = NO;
+}
+
+void use_retina_res(int enable)
+{
+	if(enable) {
+		[view_ctl enable_retina];
+	} else {
+		[view_ctl disable_retina];
+	}
+}
+
+int using_retina_res(void)
+{
+	return [view_ctl is_retina_enabled] ? 1 : 0;
+}
+
+int have_retina(void)
+{
+	if(view_ctl) {
+		return [view_ctl have_retina] ? 1 : 0;
+	}
+	return 0;
+}
+
 
 @implementation ViewController
 
@@ -27,6 +78,22 @@
 {
     [super viewDidLoad];
 
+	view_ctl = self;
+
+	NSString *str = [[UIDevice currentDevice] systemVersion];
+	if(sscanf(str.UTF8String, "%d.%d", &ios_ver_major, &ios_ver_minor) != 2) {
+		ios_ver_major = 8;
+		ios_ver_minor = 0;
+	}
+	printf("iOS version: %d.%d\n", ios_ver_major, ios_ver_minor);
+
+	// in iOS versions before 8, the view size and event coordinates are swapped in landscape
+	if((int)NSFoundationVersionNumber < NSFoundationVersionNumber_iOS_8_0) {
+		assert(ios_ver_major < 8);
+		landscape_swap = YES;
+	} else {
+		landscape_swap = NO;
+	}
 
     self->ctx = [[EAGLContext alloc] initWithAPI: kEAGLRenderingAPIOpenGLES2];
     if(!self->ctx) {
@@ -38,12 +105,13 @@
     glview.drawableDepthFormat = GLKViewDrawableDepthFormat24;
 
 	if([glview respondsToSelector: NSSelectorFromString(@"contentScaleFactor")]) {
-		pixel_scale = [[UIScreen mainScreen] scale];
-		glview.contentScaleFactor = pixel_scale;
-		printf("pixel scale: %g\n", pixel_scale);
+		max_pixel_scale = [[UIScreen mainScreen] scale];
+		printf("max pixel scale: %g\n", pixel_scale);
 	} else {
-		pixel_scale = 1.0f;
+		max_pixel_scale = 1.0f;
 	}
+	low_pixel_scale = 1.0f;
+	[self disable_retina];	// default to non-retina mode
 
 	[self create_ad];
 
@@ -52,6 +120,8 @@
 
 - (void)dealloc
 {
+	view_ctl = nil;
+
 	[self destroy_gl];
 
     if([EAGLContext currentContext] == self->ctx) {
@@ -96,8 +166,13 @@
 - (void)create_ad
 {
 	ad = [[ADBannerView alloc] initWithAdType: ADAdTypeBanner];
+	CGRect rect = ad.frame;
+	rect.size.width = glview.bounds.size.width;
+	ad.frame = rect;
 	[ad setAutoresizingMask: UIViewAutoresizingFlexibleWidth];
+
 	ad_visible = YES;
+	ad_ready = NO;
 	[self hide_ad];
 	[glview addSubview: ad];
 	ad.delegate = self;
@@ -105,7 +180,7 @@
 
 - (void)show_ad
 {
-	if(!ad_visible) {
+	if(!ad_visible && ad_ready) {
 		CGRect rect = ad.frame;
 		rect.origin.y = 0;
 		ad.frame = rect;
@@ -137,6 +212,32 @@
     [EAGLContext setCurrentContext: self->ctx];
 }
 
+- (void)enable_retina
+{
+	pixel_scale = max_pixel_scale;
+	printf("enable_retina setting pixel scale: %g\n", pixel_scale);
+	glview.contentScaleFactor = pixel_scale;
+	[glview setNeedsLayout];
+}
+
+- (void)disable_retina
+{
+	pixel_scale = low_pixel_scale;
+	printf("disable_retina setting pixel scale: %g\n", pixel_scale);
+	glview.contentScaleFactor = pixel_scale;
+	[glview setNeedsLayout];
+}
+
+- (BOOL)is_retina_enabled
+{
+	return pixel_scale > low_pixel_scale;
+}
+
+- (BOOL)have_retina
+{
+	return max_pixel_scale > 1.0;
+}
+
 
 - (void)glkView: (GLKView*)view drawInRect: (CGRect)rect
 {
@@ -148,6 +249,14 @@
 	CGRect rect = self.view.frame;
 	int xsz = rect.size.width * pixel_scale;
 	int ysz = rect.size.height * pixel_scale;
+
+	BOOL is_landscape = UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation);
+	if(is_landscape && landscape_swap) {
+		int tmp = xsz;
+		xsz = ysz;
+		ysz	= tmp;
+	}
+
 	reshape(xsz, ysz);
 	printf("viewport %dx%d (scale: %g)\n", xsz, ysz, pixel_scale);
 }
@@ -155,40 +264,57 @@
 - (void)touchesBegan: (NSSet<UITouch*>*)touches withEvent: (UIEvent*)event
 {
 	UITouch *touch = [touches anyObject];
-	CGPoint pt = [touch locationInView: nil];
-	mouse_button(0, 1, pt.x, pt.y);
+	CGPoint pt = [touch locationInView: glview];
+	int x = (int)(pt.x * pixel_scale);
+	int y = (int)(pt.y * pixel_scale);
+
+	mouse_button(0, 1, x, y);
 }
 
 - (void)touchesMoved: (NSSet<UITouch*>*)touches withEvent: (UIEvent*)event
 {
 	UITouch *touch = [touches anyObject];
-	CGPoint pt = [touch locationInView: nil];
-	mouse_motion(pt.x, pt.y);
+	CGPoint pt = [touch locationInView: glview];
+	int x = (int)(pt.x * pixel_scale);
+	int y = (int)(pt.y * pixel_scale);
+
+	mouse_motion(x, y);
 }
 
 - (void)touchesEnded: (NSSet<UITouch*>*)touches withEvent: (UIEvent*)event
 {
 	UITouch *touch = [touches anyObject];
-	CGPoint pt = [touch locationInView: nil];
-	mouse_button(0, 0, pt.x, pt.y);
+	CGPoint pt = [touch locationInView: glview];
+	int x = (int)(pt.x * pixel_scale);
+	int y = (int)(pt.y * pixel_scale);
+
+	mouse_button(0, 0, x, y);
+	printf("touch release %d %d\n", x, y);
 }
 
 - (void)touchesCancelled: (NSSet<UITouch*>*)touches withEvent: (UIEvent*)event
 {
 	UITouch *touch = [touches anyObject];
-	CGPoint pt = [touch locationInView: nil];
-	mouse_button(0, 0, pt.x, pt.y);
+	CGPoint pt = [touch locationInView: glview];
+	int x = (int)(pt.x * pixel_scale);
+	int y = (int)(pt.y * pixel_scale);
+
+	mouse_button(0, 0, x, y);
 }
 
 // ADBannerDelegate functions
 
 - (void)bannerViewDidLoadAd: (ADBannerView*)banner
 {
-	[self show_ad];
+	ad_ready = YES;
+	if(ad_show_requested) {
+		[self show_ad];
+	}
 }
 
 - (void)bannerView: (ADBannerView*)banner didFailToReceiveAdWithError: (NSError*)error
 {
+	ad_ready = NO;
 	[self hide_ad];
 
 	NSLog(@"Failed to retrieve ad");
--- src/ui.cc
+++ src/ui.cc
@@ -7,15 +7,24 @@
 
 using namespace goatkit;
 
-extern int stereo;
-extern int show_opt;
-extern int use_bump;
-extern float split;
+extern "C" void ad_banner_show(void);
+extern "C" void ad_banner_hide(void);
+extern "C" void use_retina_res(int enable);
+extern "C" int using_retina_res(void);
+extern "C" int have_retina(void);
 
 static void done_bn_handler(Widget *w, const Event &ev, void *cls);
 static void stereo_cbox_handler(Widget *w, const Event &ev, void *cls);
 static void bump_cbox_handler(Widget *w, const Event &ev, void *cls);
 static void split_slider_handler(Widget *w, const Event &ev, void *cls);
+#ifdef IPHONE
+static void retina_cbox_handler(Widget *w, const Event &ev, void *cls);
+#endif
+
+extern int stereo;
+extern int show_opt;
+extern int use_bump;
+extern float split;
 
 static Screen scr;
 static float aspect;
@@ -28,16 +37,33 @@ extern unsigned int prog_color, prog_ui;
 
 int ui_init(void)
 {
-	float ypos = 300;
-	float vsep = 70;
-	float vsz = 35;
+	float ypos = 280;
+	float vsep = 75;
+	float vsz = 36;
 
-	CheckBox *cbox = new CheckBox;
+	CheckBox *cbox;
+
+#ifdef IPHONE
+	if(have_retina()) {
+		cbox = new CheckBox;
+		cbox->set_position(300, ypos + vsep);
+		cbox->set_size(300, vsz);
+		cbox->set_text("Retina quality rendering");
+		if(using_retina_res()) {
+			cbox->check();
+		} else {
+			cbox->uncheck();
+		}
+		cbox->set_callback(EV_CHANGE, retina_cbox_handler);
+		scr.add_widget(cbox);
+	}
+#endif
+
+	cbox = new CheckBox;
 	cbox->set_position(300, ypos);
 	cbox->set_size(300, vsz);
 	cbox->set_text("Stereoscopic rendering");
 	cbox->set_callback(EV_CHANGE, stereo_cbox_handler);
-	//cbox->set_toggle_transition(80);
 	scr.add_widget(cbox);
 	ypos -= vsep;
 
@@ -67,44 +93,16 @@ int ui_init(void)
 	scr.add_widget(cbox);
 	ypos -= vsep;
 
-	ypos -= vsep * 0.5;
+	ypos -= vsep * 0.25;
 	Button *button = new Button;
 	button->set_position(450, ypos);
 	button->set_size(150, vsz);
-	button->set_text("Done");
+	button->set_text("Ok");
 	button->set_callback(EV_CLICK, done_bn_handler);
 	scr.add_widget(button);
 	ypos -= vsep;
 
-	/*
-	TextBox *text = new TextBox;
-	text->set_position(300, ypos += vsep);
-	text->set_size(200, 30);
-	text->set_text("foo");
-	text->set_callback(EV_CHANGE, callback);
-	scr.add_widget(text);
-
-	CheckBox *cbox = new CheckBox;
-	cbox->set_position(300, ypos += vsep);
-	cbox->set_size(200, 20);
-	cbox->set_text("a checkbox!");
-	cbox->set_callback(EV_CHANGE, callback);
-	scr.add_widget(cbox);
-
-	Slider *intslider = new Slider;
-	intslider->set_position(300, ypos += vsep);
-	intslider->set_size(200, 40);
-	intslider->set_callback(EV_CHANGE, callback);
-	intslider->set_continuous_change(false);
-	intslider->set_range(0, 100.0);
-	intslider->set_step(10);
-	scr.add_widget(intslider);
-	 */
-
 	scr.set_visibility_transition(400);
-
-	// load the theme
-	//add_theme_path("themes/simple");
 
 	if(!(theme = get_theme("istereo"))) {
 		return -1;
@@ -120,11 +118,13 @@ void ui_shutdown(void)
 void ui_show()
 {
 	scr.show();
+	ad_banner_show();
 }
 
 void ui_hide()
 {
 	scr.hide();
+	ad_banner_hide();
 }
 
 int ui_visible()
@@ -227,4 +227,11 @@ static void split_slider_handler(Widget *w, const Event &ev, void *cls)
 {
 	split = ((Slider*)w)->get_value();
 }
+
+#ifdef IPHONE
+static void retina_cbox_handler(Widget *w, const Event &ev, void *cls)
+{
+	use_retina_res(((CheckBox*)w)->is_checked() ? 1 : 0);
+}
+#endif
 
